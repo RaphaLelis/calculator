4955da17433cf07ca0260076a1f7e6d86f17d644
Raphael Lelis <raphalelis@outlookc.com>
2022-07-26 15:03:39 -0300

    init calculator

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Calculator.css'
+const Calculator: React.FC = () => {
+  return (
+		<div>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+		</div>
+	);
+}
+
+export default Calculator;
